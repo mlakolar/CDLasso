e477a14aa38c2de3a8b3a61e772f694968f04f2a
Mladen Kolar <mkolar@gmail.com>
2015-05-08 23:02:21 -0500

    feasible lasso

--- src/CDLasso.jl
+++ src/CDLasso.jl
@@ -1,5 +1,7 @@
 module CDLasso
 
+import Base.LinAlg.BLAS
+
 #######################################################################
 type LassoOptions
   max_inner_iter::Int64
@@ -141,16 +143,10 @@ function _lasso!{T<:FloatingPoint}(
       j = indexes[i]
       a = A[j, j]
       S0 = _Axk(A, x, j, activeset) - a * x[j] + b[j]
-      @show S0
       newValue = shrink(-S0 / a, abs(λ[j] / a))
-#       if abs(newValue) > 1e4
-#         @show S0, λ[j], i, j, A[j,j], activeset
-#         error("agg")
-#       end
       if abs(x[j] - newValue) > maxUpdate
         maxUpdate = abs(x[j] - newValue)
       end
-      @show j, newValue
       x[j] = newValue
     end
     if iter > options.max_inner_iter || maxUpdate < options.xtol
@@ -172,20 +168,18 @@ function lasso!{T<:FloatingPoint}(
     x::StridedVector{T},
     A::StridedMatrix{T},
     b::StridedVector{T},
-    λ::Vector{T};
+    λ::StridedVector{T};
     options::LassoOptions   =   LassoOptions()
     )
+
   maxoutiter = options.max_outer_iter
   activeset = ActiveSet(x; options=options)
-
   if activeset.numActive == 0
     _add_violator!(activeset, x, A, b, λ; options=options)
   end
 
-  @show activeset
   for outiter=1:maxoutiter
     _lasso!(x, A, b, λ, activeset; options=options)
-    @show x
     if ~_add_violator!(activeset, x, A, b, λ; options=options)
       break
     end
@@ -193,8 +187,87 @@ function lasso!{T<:FloatingPoint}(
   x, activeset
 end
 
+
+function _penalty_loadings!{T<:FloatingPoint}(
+    γ::StridedVector{T},
+    λ::StridedVector{T},
+    X::StridedMatrix{T},
+    res::StridedVector{T}
+    )
+  n, p = size(X)
+  for j=1:p
+    s = zero(T)
+    m = zero(T)
+    for i=1:n
+      s += X[i, j]^2. * res[i]^2.
+      m += X[i, j] * res[i]
+    end
+    γ[j] = sqrt(s / n - (m/n)^2.) * λ[j]
+  end
+end
+
+# numIterations that estimate Gamma matrix
+function feasible_lasso!{T<:FloatingPoint}(
+    x::StridedVector{T},
+    X::StridedMatrix{T},
+    Y::StridedVector{T},
+    λ::Vector{T};
+    options::LassoOptions   =   LassoOptions(),
+    refit::Bool             =   true,
+    numIterations::Int      =   1,
+    xtol::T                 =   1e-4
+    )
+
+  n, p = size(X)
+  A = X'X
+  scale!(A, 1./n)
+  b = X'Y
+  scale!(b, -1./n)
+
+  xold = copy(x)
+  γ = copy(λ)
+  res = copy(Y)
+
+  BLAS.gemv!('N', -1., X, x, 1., res)
+  _penalty_loadings!(γ, λ, X, res)
+
+  for iter=1:numIterations
+    _, activeset = lasso!(x, A, b, γ)
+
+    # check if done
+    s = zero(T)
+    for j=1:p
+      s = max(s, abs(x[j] - xold[j]))
+    end
+    if s < xtol
+      break
+    end
+
+    if refit
+      indNZ = activeset.indexes[1:activeset.numActive]
+      #       xv = sub(x, indNZ)
+      #       Av = sub(A, indNZ, indNZ)
+      #       bv = sub(b, indNZ)
+      #       xv[:] = Av \ bv
+      #       scale!(xv, -1)
+      x[indNZ] = X[:, indNZ] \ Y[:]
+    end
+
+    # update γ
+    if iter < numIterations
+      # re-estimate gamma unless last iteration
+      copy!(res, Y)
+      BLAS.gemv!('N', -1., X, x, 1., res)
+      _penalty_loadings!(γ, λ, X, res)
+
+      copy!(xold, x)
+    end
+    #
+  end
+  x
+end
+
 ### todo:
-# feasible lasso
 # groups lasso
 
 
